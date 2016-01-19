3460a760d45db8f1c85a01ff8a10500907183804
Mustafa Arslan <arslan.mustafa@windowslive.com>
2016-01-19 09:35:25 +0200

    Project initial commit

---
+++ Macro.Common.API/Excel/Excel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace Macro.Common.API.Excel
+{
+    public class Excel
+    {
+        public Excel()
+        {
+
+        }
+
+        public System.Data.DataTable Read(string path)
+        {
+            Application _excelApp = new Application();
+
+            //open the workbook
+            Workbook workbook = _excelApp.Workbooks.Open(path,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            //select the first sheet        
+            Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+
+            //find the used range in worksheet
+            Range excelRange = worksheet.UsedRange;
+
+            //get an object array of all of the cells in the worksheet (their values)
+            object[,] valueArray = (object[,])excelRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+
+            System.Data.DataTable records = new System.Data.DataTable();
+
+            //access the cells
+            for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+            {
+                DataRow dataRow = records.NewRow();
+                for (int col = 1; col <= worksheet.UsedRange.Columns.Count; ++col)
+                {
+                    if (row == 1)
+                        records.Columns.Add(valueArray[row, col].ToString().ToUpper().Trim(), valueArray[row, col].GetType());
+                    else
+                        dataRow[col - 1] = valueArray[row, col];
+                }
+
+                if (row > 1)
+                    records.Rows.Add(dataRow);
+            }
+
+            //clean up stuffs
+            workbook.Close(false, Type.Missing, Type.Missing);
+            Marshal.ReleaseComObject(workbook);
+
+            _excelApp.Quit();
+            Marshal.FinalReleaseComObject(_excelApp);
+
+            return records;
+        }
+    }
+}
---
+++ Macro.Common.API/Excel/ExcelListExtension.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Drawing;
+
+namespace Macro.Common.API.Excel
+{
+    public static class ExcelListExtension
+    {
+        public static void ToExcel<T>(this List<T> list, string pathToSave)
+        {
+            #region Declarations
+
+            if (string.IsNullOrEmpty(pathToSave))
+            {
+                throw new Exception("Invalid file path.");
+            }
+            else if (pathToSave.ToLower().Contains("") == false)
+            {
+                throw new Exception("Invalid file path.");
+            }
+
+            if (list == null)
+            {
+                throw new Exception("No data to export.");
+            }
+
+            Microsoft.Office.Interop.Excel.Application excelApp = null;
+            Microsoft.Office.Interop.Excel.Workbooks books = null;
+            Microsoft.Office.Interop.Excel._Workbook book = null;
+            Microsoft.Office.Interop.Excel.Sheets sheets = null;
+            Microsoft.Office.Interop.Excel._Worksheet sheet = null;
+            Microsoft.Office.Interop.Excel.Range range = null;
+            Microsoft.Office.Interop.Excel.Font font = null;
+            // Optional argument variable
+            object optionalValue = Missing.Value;
+
+            string strHeaderStart = "A2";
+            string strDataStart = "A3";
+            #endregion
+
+            #region Processing
+
+            try
+            {
+                #region Init Excel app.
+
+                excelApp = new Microsoft.Office.Interop.Excel.Application();
+                books = excelApp.Workbooks;
+                book = books.Add(optionalValue);
+                sheets = book.Worksheets;
+                sheet = (Microsoft.Office.Interop.Excel._Worksheet)(sheets.get_Item(1));
+
+                #endregion
+
+                #region Creating Header
+
+                Dictionary<string, string> objHeaders = new Dictionary<string, string>();
+
+                PropertyInfo[] headerInfo = typeof(T).GetProperties();
+
+                foreach (var property in headerInfo)
+                {
+                    var attribute = property.GetCustomAttributes(typeof(DisplayNameAttribute), false)
+                                            .Cast<DisplayNameAttribute>().FirstOrDefault();
+
+                    objHeaders.Add(property.Name, attribute == null ?
+                                        property.Name : attribute.DisplayName);
+                }
+
+                range = sheet.get_Range(strHeaderStart, optionalValue);
+                range = range.get_Resize(1, objHeaders.Count);
+
+                range.set_Value(optionalValue, objHeaders.Values.ToArray());
+                range.BorderAround(Type.Missing, Microsoft.Office.Interop.Excel.XlBorderWeight.xlThin, Microsoft.Office.Interop.Excel.XlColorIndex.xlColorIndexAutomatic, Type.Missing);
+
+                font = range.Font;
+                font.Bold = true;
+                range.Interior.Color = Color.LightGray.ToArgb();
+
+                #endregion
+
+                #region Writing data to cell
+
+
+                int count = list.Count;
+                object[,] objData = new object[count, objHeaders.Count];
+
+                for (int j = 0; j < count; j++)
+                {
+                    var item = list[j];
+                    int i = 0;
+                    int k = 0;
+                    foreach (KeyValuePair<string, string> entry in objHeaders)
+                    {
+                        var y = typeof(T).InvokeMember(entry.Key.ToString(), BindingFlags.GetProperty, null, item, null);
+
+                        if (k == 0)
+                        {
+                            objData[j, i++] = "";
+                            k++;
+                            continue;
+                        }
+
+                        if (y != null && y is decimal)
+                        {
+                            decimal d = (decimal)y;
+                            objData[j, i++] = (y == null) ? "" : d.ToString("#,#.00#");
+
+                            continue;
+                        }
+                        
+                        objData[j, i++] = (y == null) ? "" : y.ToString();
+
+                    }
+                }
+
+
+                range = sheet.get_Range(strDataStart, optionalValue);
+                range = range.get_Resize(count, objHeaders.Count);
+
+                range.set_Value(optionalValue, objData);
+                range.BorderAround(Type.Missing, Microsoft.Office.Interop.Excel.XlBorderWeight.xlThin, Microsoft.Office.Interop.Excel.XlColorIndex.xlColorIndexAutomatic, Type.Missing);
+
+                range = sheet.get_Range(strHeaderStart, optionalValue);
+                range = range.get_Resize(count + 1, objHeaders.Count);
+                range.Columns.AutoFit();
+
+                #endregion
+
+                #region Saving data and Opening Excel file.
+
+
+                if (string.IsNullOrEmpty(pathToSave) == false)
+                    book.SaveAs(pathToSave);
+
+                excelApp.Visible = true;
+
+                #endregion
+
+                #region Release objects
+
+                try
+                {
+                    if (sheet != null)
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
+                    sheet = null;
+
+                    if (sheets != null)
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(sheets);
+                    sheets = null;
+
+                    if (book != null)
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(book);
+                    book = null;
+
+                    if (books != null)
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(books);
+                    books = null;
+
+                    if (excelApp != null)
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                    excelApp = null;
+                }
+                catch (Exception ex)
+                {
+                    sheet = null;
+                    sheets = null;
+                    book = null;
+                    books = null;
+                    excelApp = null;
+                }
+                finally
+                {
+                    GC.Collect();
+                }
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            #endregion
+        }
+    }
+}
